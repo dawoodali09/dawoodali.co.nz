056f3e7cc49d4054757714c0481f44a14f4ebeff
dawoodali09 <dawoodali@gmail.com>
2025-07-17 17:45:25 +1200

    updating

--- Pages/ProjectsAndCaseStudies.cshtml.cs
+++ Pages/ProjectsAndCaseStudies.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.RazorPages; 
+using Newtonsoft.Json;
+using System.Collections.Generic;
+ 
+
+
 
 namespace dawoodali.co.nz.Pages
 {
     public class ProjectsAndCaseStudiesModel : PageModel
     {
+
+        public List<Dictionary<string, object>> Projects { get; private set; }
+
         public void OnGet()
         {
+            // Path to your JSON file
+            var jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/assets/Files", "projects.json");
+
+            // Check if the file exists and read the data
+            if (System.IO.File.Exists(jsonFilePath))
+            {
+                var jsonData = System.IO.File.ReadAllText(jsonFilePath);
+                Projects = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(jsonData);
+            }
         }
     }
 }
